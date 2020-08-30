5297aa2d1e71bd1811974773f0563746e08e4583
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-08-30 20:42:26 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
-class App extends Component {   
-  
+class App extends Component {
+
   render() {
     return (
       <div>
@@ -20,4 +20,3 @@ const mapStateToProps = state => {
 }
 
 export default connect(mapStateToProps)(App)
-
