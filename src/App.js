1f75f1d31121a9e4cadcc384fd2d1b946d53712c
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-08-30 20:56:49 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchCats } from './actions/catActions'
+import CatList from './CatList'
 
 class App extends Component {
 
